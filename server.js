7e983bc38d57c8cc9bc4c755019b999973341d42
mohamedh2000 <mohamed.h@northeastern.edu>
2022-07-09 16:38:19 -0400

    added env file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
+require('dotenv').config();
 const SerpApi = require('google-search-results-nodejs');
-const search = new SerpApi.GoogleSearch("d5be2cab30a3eab6f8b63046403408a90a38a8bdb33fe26396b20debec8afa17");
-
+const search = new SerpApi.GoogleSearch(process.env.API_KEY);
 
 const app = express()
 const port = 3000
@@ -20,7 +20,6 @@ const callback = function(data) {
 };
 
 // Show result as JSON
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
