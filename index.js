12a698e9ff9ed93e1fddd2ee6d3008e83a7e9c81
elongateddmuskrat <60983022+elongateddmuskrat@users.noreply.github.com>
2022-07-09 16:04:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$("form").submit(function (e) {
+    e.preventDefault()
+    var query = $("#search").val()
+
+    var API_KEY = 'f5f717749812d8d2fa974f1c0d11370ca039e4848e2731225e0679cfbc636a5d'
+
+    var url = 'https://serpapi.com/search.json?engine=google&q=${query}&api_key=${API_KEY}'
+    
+    $.get(url, function (data) {
+
+        console.log(data)
+    })
+    
+})
